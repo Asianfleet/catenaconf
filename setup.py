e342efa5d8e04ca4a7ebd37b447f8a1797493bc3
Asianfleet <1239326528@qq.com>
2025-01-06 02:07:56 +0800

    github action 配置更新 setup.py 更新

--- setup.py
+++ setup.py
@@ -2,24 +2,29 @@
 import io
 from setuptools import setup, find_packages
 
-with io.open('README.md', encoding='utf-8') as f:
+# 读取README.md文件
+with io.open("README.md", encoding="utf-8") as f:
     long_description = f.read()
     
+# 读取requirements.txt文件
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
+    
 setup(
-    name='catenaconf',
-    version='0.1.0',
+    name="catenaconf",
+    version="0.1.0",
     packages=find_packages(),
-    install_requires=[],
-    description='A configuration management library',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    author='Jianqing Wang',
-    author_email='18353419066@163.com',
-    url='https://github.com/Asianfleet/catenaconf',
+    install_requires=required,
+    description="A python configuration management library based on dict, with similar grammar as omegaconf",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Jianqing Wang",
+    author_email="18353419066@163.com",
+    url="https://github.com/Asianfleet/catenaconf",
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires=">=3.6",
 )
